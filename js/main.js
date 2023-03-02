da8fa0681d49fb656fc9bee3d5a599bfe3dc84cb
albertogioia93 <albertogioia@hotmail.it>
2023-03-02 16:50:20 +0100

    manipolazione del dom per creare celle con js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 'use strict';
 
+function creareCella(element, className, htmlValore){
+   const cellaCreata = document.createElement(element);
+   cellaCreata.classList.add(className);
+   cellaCreata.innerText = htmlValore;
+
+   cellaCreata.addEventListener('click', function() {
+       alert(htmlValore);
+   });
+
+   return cellaCreata;
+}
+
+function elementoAppeso(containerElement, htmlElement){
+    containerElement.append(htmlElement);
+}
+
+
+const contenitore = document.querySelector('.container');
+
+for(let i = 1; i <= 100; i++){
+    const elementoCreato = creareCella('div', 'cella', i);
+    elementoAppeso(contenitore, elementoCreato);
+}
+
+
+
+// const bottoneClick = document.querySelector('button');
+
+// bottoneClick.addEventListener('click', function() {
+//     alert('bottone cliccato');
+// });
